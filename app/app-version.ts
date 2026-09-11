c294d06bf3c9c2f8e5fdb0cac9b69e6cd25b15fd
Asad Amanat Ali <270855559+bc878vu@users.noreply.github.com>
2026-09-11 23:20:52 +0500

    chore(release): update internal admin studio release

--- app/app-version.ts
+++ app/app-version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = '0.8.8';
-export const RELEASE_NAME = 'Responsive CV Studio';
+export const APP_VERSION = '0.9.0';
+export const RELEASE_NAME = 'Admin Control Studio';
 export const RELEASE_DATE = 'September 11, 2026';
